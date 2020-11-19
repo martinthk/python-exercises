ce12bdb86b26a8faf8842e785c8e8b2b6ad58b45
Martin Tom <73326875+martinthk@users.noreply.github.com>
2020-11-19 23:08:37 +0800

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,13 @@
+# Ex2: Input
+x = input('Enter the value for x: ')
+y = input('Enter the vakue for y: ')
+x = int(x)
+y = int(y)
+#x= int(input ('Enter the value for x: '))
+
+print(x, '+', y, '=', x+y)
+print(x, '-', y, '=', x-y)
+print(x, '*', y, '=', x*y)
+print(x, '/', y, '=', x/y)
+print(x, '//', y, '=', x//y)
+print(x, '%', y, '=', x % y)
