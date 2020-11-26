59ea67c91bae895ccbcbfb122af0f71ca50b2bd6
Martin Tom <73326875+martinthk@users.noreply.github.com>
2020-11-26 21:11:51 +0800

    Add files via upload

---
+++ code/Ex6_CountVowels.py
@@ -0,0 +1,10 @@
+# Ex6: Counting vowels
+
+s = input('Enter your name: ') # Takes as input a string representing a user's name
+s = s.lower()
+print('Length of your name is: ', len(s))
+print("The number of a's are ", s.count('a'))
+print("The number of e's are ", s.count('e'))
+print("The number of i's are ", s.count('i'))
+print("The number of o's are ", s.count('o'))
+print("The number of u's are ", s.count('u'))
