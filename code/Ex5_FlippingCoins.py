5b3e3de761d737030aa97aff5eff20ed92080f78
Martin Tom <73326875+martinthk@users.noreply.github.com>
2020-11-26 21:11:17 +0800

    Add files via upload

---
+++ code/Ex5_FlippingCoins.py
@@ -0,0 +1,62 @@
+# Ex5: Flipping coins
+
+import random
+
+# Takes a value p, with range between 0 and 1, 
+#   as input from the user and tests a coin flip 
+#   where p is the probability of the flip resulting in heads.
+p = float(input('Enter a value for p-probability of heads (range: 0..1): '))
+
+# Print true if the coin flip results in a head and false 
+#   if the coin flip results in a tail.
+
+# Generates and displays the result 10 times
+r = random.random()
+print('The random number generated is: ', r)
+print('Output (Heads is True, Tails is False):', r > p)
+print('\n')
+
+r = random.random()
+print('The random number generated is: ', r)
+print('Output (Heads is True, Tails is False):', r > p)
+print('\n')
+
+r = random.random()
+print('The random number generated is: ', r)
+print('Output (Heads is True, Tails is False):', r > p)
+print('\n')
+
+r = random.random()
+print('The random number generated is: ', r)
+print('Output (Heads is True, Tails is False):', r > p)
+print('\n')
+
+r = random.random()
+print('The random number generated is: ', r)
+print('Output (Heads is True, Tails is False):', r > p)
+print('\n')
+
+r = random.random()
+print('The random number generated is: ', r)
+print('Output (Heads is True, Tails is False):', r > p)
+print('\n')
+
+r = random.random()
+print('The random number generated is: ', r)
+print('Output (Heads is True, Tails is False):', r > p)
+print('\n')
+
+r = random.random()
+print('The random number generated is: ', r)
+print('Output (Heads is True, Tails is False):', r > p)
+print('\n')
+
+r = random.random()
+print('The random number generated is: ', r)
+print('Output (Heads is True, Tails is False):', r > p)
+print('\n')
+
+r = random.random()
+print('The random number generated is: ', r)
+print('Output (Heads is True, Tails is False):', r > p)
+print('\n')
